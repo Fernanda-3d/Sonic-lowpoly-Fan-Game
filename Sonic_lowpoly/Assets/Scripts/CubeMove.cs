43ef991728feb64924a7b945e312badb83218011
Fernanda-3d <81561132+Fernanda-3d@users.noreply.github.com>
2021-06-15 21:26:27 +0100

    15-06-2021

--- Sonic_lowpoly/Assets/Scripts/CubeMove.cs
+++ Sonic_lowpoly/Assets/Scripts/CubeMove.cs
@@ -17,9 +17,9 @@ public class CubeMove : MonoBehaviour
     void Update()
     {
         //Moves Forward and back along z axis                           //Up/Down
-        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+        transform.Translate(-Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
         //Moves Left and right along x Axis                               //Left/Right
-       transform.Translate(-Vector3.right * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+       transform.Translate(-Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
     }
 
 
