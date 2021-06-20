6242a41ad98adb5ed2880937363aaaf201bac945
Fernanda-3d <81561132+Fernanda-3d@users.noreply.github.com>
2021-06-20 18:27:16 +0100

    20-06-2021

--- Sonic_lowpoly/Assets/Scripts/Clouds_inst.cs
+++ Sonic_lowpoly/Assets/Scripts/Clouds_inst.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Clouds_inst : MonoBehaviour
 {
 
-    public GameObject clouds;
+    public GameObject cloud1;
+    public GameObject cloud2;
     public int xPos;
     public int yPos;
     public int zPos;
+
+    public int xPos2;
+    public int yPos2;
+    public int zPos2;
+
+   // float minScale = 0.25f;
+   // float maxScale = 1.5f;
 
     public int cloudCount;
 
@@ -22,21 +30,36 @@ public class Clouds_inst : MonoBehaviour
     }
     IEnumerator CloudDrop()
     {
-      /*  temp = transform.localScale;
-        temp.x += 0.05f;
-        temp.y += 0.05f;
-        temp.z += 0.05f;
-        transform.localScale = temp; */
-
-        while (cloudCount < 100)
+      
+        while (cloudCount < 150)
         {
-            xPos = Random.Range(-36, 108);
-            yPos = Random.Range(1, 5);
-            zPos = Random.Range(1, 5);
-            Instantiate(clouds, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+            xPos = Random.Range(15, -230);
+            yPos = Random.Range(10, 50);
+            zPos = Random.Range(-125, -175);
+            
+
+            xPos2 = Random.Range(10, -200);
+            yPos2 = Random.Range(20, 55);
+            zPos2 = Random.Range(-130, -150);
+
+           GameObject cloud_1 = Instantiate(cloud1, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+           GameObject cloud_2 = Instantiate(cloud2, new Vector3(xPos2, yPos2, zPos2), Quaternion.identity);
+
+            
+            cloud1.transform.localScale = new Vector3 (Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f));
+            cloud2.transform.localScale = new Vector3(Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f));
+
+            // transform.localScale *= Random.Range(minScale, maxScale);
+
+            /* temp = transform.localScale;
+             temp.x *= Random.Range(1, 5);
+             temp.y *= Random.Range(1, 5);
+             temp.z *= Random.Range(1, 55);
+             transform.localScale = temp;*/
 
             yield return new WaitForSeconds(0.01f);
             cloudCount += 1;
+
         }
 
 
